8a2583519f7e286731de3828253e081e86856db2
Eran <eran.hasarali.2001@gmail.com>
2025-02-22 18:03:40 +0530

    config some files

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ import client from "./db/db.js";
 const app = express();
 const PORT = process.env.PORT;
 
-await client.connect();
+console.clear();
+
+app.use(express.json());
+
+client
+  .connect()
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.error("Database connection error:", err));
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
